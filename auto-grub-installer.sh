531b491b3e9545d0c76784cf228d6c54ac011a63
Nick Brady <nbgenius1@gmail.com>
2021-10-01 09:36:30 +0000

    Update auto-grub-installer.sh

--- auto-grub-installer.sh
+++ auto-grub-installer.sh
@@ -4,13 +4,12 @@ NC='\033[0m'
 RED='\033[0;31m'
 
 if [[ $(fdisk "-l" | awk '/Disklabel/*/type:/ { print $3 }') == 'dos' ]]; then
-        echo "grub-install --target=i386-pc $(fdisk "-l" | awk '/dev/*/\*/ { print $1 }')"
+        grub-install --target=i386-pc $(fdisk "-l" | awk '/dev/*/\*/ { print $1 }'
+        grub-mkconfig -o /boot/grub/grub.cfg
 elif [[ $(fdisk "-l" | awk '/Disklabel/*/type:/ { print $3 }') == 'gpt' ]]; then
         var=$(sudo fdisk "-l" | awk '/EFI/*/\System/ { print $1 }' | cut -c6-)
-        loc=$(lsblk "-rno" name,mountpoint | awk -v pat="$var" '$0 ~ pat { print $2}')
-        #fs=$(cat joe.txt)
-        echo "grub-install --target=x86_64-efi --efi-directory=$loc --bootloader-id=GRUB"
-        #echo "fuck this shit"
+        grub-install --target=x86_64-efi --efi-directory=$(lsblk "-rno" name,mountpoint | awk -v pat="$var" '$0 ~ pat { print $2}') --bootloader-id=GRUB
+        grub-mkconfig -o /boot/grub/grub.cfg
 else
         echo -e "${RED}""Something has gone wrong chief ¯\_(ツ)_/¯""${NC}"
 fi
