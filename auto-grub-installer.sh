1813f470ac4af77545c33a45e0555563cabbed56
Nick Brady <nbgenius1@gmail.com>
2021-10-01 14:28:20 +0000

    Update auto-grub-installer.sh

--- auto-grub-installer.sh
+++ auto-grub-installer.sh
@@ -7,6 +7,7 @@ if [[ $(fdisk "-l" | awk '/Disklabel/*/type:/ { print $3 }') == 'dos' ]]; then
         grub-install --target=i386-pc $(fdisk "-l" | awk '/dev/*/\*/ { print $1 }'
         grub-mkconfig -o /boot/grub/grub.cfg
 elif [[ $(fdisk "-l" | awk '/Disklabel/*/type:/ { print $3 }') == 'gpt' ]]; then
+        pacman -S --noconfirm efibootmgr
         grub-install --target=x86_64-efi --efi-directory=$(lsblk "-rno" name,mountpoint | awk -v pat="$(fdisk "-l" | awk '/EFI/*/\System/ { print $1 }' | cut -c6-)" '$0 ~ pat { print $2}') --bootloader-id=GRUB
         grub-mkconfig -o /boot/grub/grub.cfg
 else
