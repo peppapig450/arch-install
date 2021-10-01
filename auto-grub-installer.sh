386ef3e94d96939d05457e1b6081b72259ef41ef
Nick Brady <nbgenius1@gmail.com>
2021-10-01 09:37:27 +0000

    remove sudo fdisk from gpt

--- auto-grub-installer.sh
+++ auto-grub-installer.sh
@@ -7,7 +7,7 @@ if [[ $(fdisk "-l" | awk '/Disklabel/*/type:/ { print $3 }') == 'dos' ]]; then
         grub-install --target=i386-pc $(fdisk "-l" | awk '/dev/*/\*/ { print $1 }'
         grub-mkconfig -o /boot/grub/grub.cfg
 elif [[ $(fdisk "-l" | awk '/Disklabel/*/type:/ { print $3 }') == 'gpt' ]]; then
-        var=$(sudo fdisk "-l" | awk '/EFI/*/\System/ { print $1 }' | cut -c6-)
+        var=$(fdisk "-l" | awk '/EFI/*/\System/ { print $1 }' | cut -c6-)
         grub-install --target=x86_64-efi --efi-directory=$(lsblk "-rno" name,mountpoint | awk -v pat="$var" '$0 ~ pat { print $2}') --bootloader-id=GRUB
         grub-mkconfig -o /boot/grub/grub.cfg
 else
